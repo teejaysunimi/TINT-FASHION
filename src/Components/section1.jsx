879273083534ee89e78dadce95e6ef458f9406f6
Tijesunimi Emmanuel <tijesunimiemmanuel@gmail.com>
2026-04-28 07:58:39 +0100

    Section 1 done

--- src/Components/section1.jsx
+++ src/Components/section1.jsx
@@ -3,17 +3,22 @@ import rrr from '../assets/images/rrr.jpg'
 import OOM from '../assets/images/OOM.jpg'
 export const Section = () => {
   return (
-    <div className="bg-black block  text-white">
-      <h2 className="justify-items-center pt-5 pl- ">OUR LIMITED COLLECTIONS</h2>
-      <div className="flex">
-        <div className="max-w-max relative">
-          <img className='w-[40%] ' src={odn} alt="" />
+    <div className="bg-black block text-center pt-4 text-white">
+      <h2 className="text-4xl font-bold pb-5">OUR LIMITED COLLECTIONS</h2>
+      <div className="flex justify-center items-start p-4 gap-5">
+        <div className="w-[40%] ">
+          <img className="" src={odn} alt="odn" />
         </div>
-        <div className="">
-          <img className='w-[40%]' src={rrr} alt="" />
+        <div className="w-[40%] relative inline-block">
+          <img className="" src={rrr} alt="rrr" />
+          <div className="absolute bottom-60 left-34 max-w">
+            <a href="">
+              <p className='border-2 text-2xl pl-4 pr-4 hover:bg-black'>SHOP ALL</p>
+            </a>
+          </div>
         </div>
-        <div className="">
-          <img className='w-[40%]' src={OOM} alt="" />
+        <div className="w-[40%]">
+          <img className="" src={OOM} alt="OOOM" />
         </div>
       </div>
     </div>
